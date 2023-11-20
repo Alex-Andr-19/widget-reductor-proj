7ef9078beef0b3c02e28f26c56c325ccae844b20
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2023-11-20 11:23:54 +0300

    Create canvas + customRect class

--- src/pages/mainPage/mainPage.vue
+++ src/pages/mainPage/mainPage.vue
@@ -10,6 +10,10 @@
             <div class="mainPage__mainBlock__title">
                 Редактор
             </div>
+
+            <div ref="canvasContainer" class="mainPage__mainBlock__canvasContainer">
+                <canvas id="canvasEditor"></canvas>
+            </div>
         </div>
 
         <div class="mainPage__sideBlock">
@@ -21,7 +25,65 @@
 </template>
 
 <script setup>
-    console.log("Here!!!");
+import { ref, onMounted } from "vue";
+import customRect from '@/helpers/customRect'
+
+const canvasContainer = ref(null);
+const canvas = ref(undefined);
+const ctx = ref(undefined);
+
+const firstCustomRect = ref(undefined);
+
+function mountFunction() {
+    canvas.value = document.getElementById("canvasEditor");
+    ctx.value = canvas.value.getContext("2d");
+
+    const colors = ["#000", "#F00", "#0F0", "#00F"];
+    const defaultWidth = 150;
+    const defaultHeight = 100;
+    const gap = 20;
+
+    for (let i in colors) {
+        const sizing = {
+            x: 20 + (defaultWidth + gap) * i,
+            y: 20,
+            w: defaultWidth,
+            h: defaultHeight
+        }
+
+        const styles = {
+            bc: colors[i],
+            r: (i + 1) * 2
+        }
+
+        new customRect(canvas.value, {
+            sizing,
+            styles
+        });
+    }
+}
+
+function createRoundRect(x, y, w, h, c, r = 0) {
+    ctx.value.beginPath();
+    ctx.value.roundRect(x, y, w, h, r);
+    ctx.value.strokeStyle = c;
+    ctx.value.lineWidth = 1;
+    ctx.value.stroke();
+}
+
+function mountCanvas() {
+    const canvasTag = document.getElementById("canvasEditor");
+    const editorWidth = canvasContainer.value.clientWidth;
+    const editorHeight = canvasContainer.value.clientHeight;
+
+    canvasTag.setAttribute("width", editorWidth);
+    canvasTag.setAttribute("height", editorHeight);
+}
+
+onMounted(() => {
+    mountCanvas();
+    mountFunction();
+})
 </script>
 
 <style scoped lang="scss" src="./style.scss"></style>
