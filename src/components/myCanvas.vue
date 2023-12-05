7c5cd55d079afdfdddf7b589c74ea8da7d20a245
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2023-12-05 14:20:25 +0300

    Create normal version of canvas by kanvaJs

---
+++ src/components/myCanvas.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="canvasContainer"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+import Konva from "konva";
+
+const stage = ref(undefined);
+
+const cameraObj = ref({
+    zoom: 1,
+    ZOOM_SENSITIVITY: 0.0005,
+    centerDelta: {
+        x: 0,
+        y: 0,
+    },
+});
+
+const coordinatesObj = ref({
+    centerDelta: {
+        x: 0,
+        y: 0,
+    },
+})
+
+function createRandomRect(e) {
+    return new Konva.Rect({
+        x: e.offsetX,
+        y: e.offsetY,
+        // x: Math.round(Math.random() * coordinatesObj.value.centerDelta.x * 2),
+        // y: Math.round(Math.random() * coordinatesObj.value.centerDelta.y * 2),
+        width: Math.round(Math.random() * 50 + 50),
+        height: Math.round(Math.random() * 50 + 50),
+        fill: 'green',
+        stroke: 'black',
+        strokeWidth: 2,
+        draggable: true,
+    });
+}
+
+function addRectToFirstLayer(e = {evt: { offsetX: 0, offsetY: 0 }}) {
+    stage.value.children[0].add(createRandomRect(e.evt));
+}
+
+function initStage() {
+    stage.value = new Konva.Stage({
+        container: "canvasContainer",
+        width: document.querySelector(".mainPage__mainBlock__canvas").clientWidth,
+        height: document.querySelector(".mainPage__mainBlock__canvas").clientHeight,
+    })
+
+    stage.value.on("click", addRectToFirstLayer)
+}
+
+function createLayer() {
+    const layer = new Konva.Layer();
+    stage.value.add(layer);
+}
+
+onMounted(() => {
+    initStage();
+    createLayer();
+
+    coordinatesObj.value.centerDelta.x = Math.round(stage.value.attrs.width / 2);
+    coordinatesObj.value.centerDelta.y = Math.round(stage.value.attrs.height / 2);
+
+    addRectToFirstLayer();
+})
+
+console.log("Created");
+</script>
+
+<style lang="scss">
+#canvasContainer {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/pages/mainPage/mainPage.vue
+++ src/pages/mainPage/mainPage.vue
@@ -11,17 +11,8 @@
                 Редактор
             </div>
 
-            <div @click="createRandomRect" class="mainPage__mainBlock__canvas">
-                <v-stage
-                    v-if="configKonva !== undefined"
-                    :config="configKonva"
-                    
-                    ref="stageRef"
-                >
-                    <v-layer>
-                        <v-rect v-for="rect in rectList" :config="rect"></v-rect>
-                    </v-layer>
-                </v-stage>
+            <div class="mainPage__mainBlock__canvas">
+                <myCanvas />
             </div>
         </div>
 
@@ -34,61 +25,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-
-const configKonva = ref(undefined);
-const configCircle = ref(undefined);
-
-const stageRef = ref(null);
-const stage = ref(undefined);
-
-const rectList = ref([]);
-
-const cameraObj = ref({
-    zoom: 1,
-    ZOOM_SENSITIVITY: 0.0005,
-    centerDelta: {
-        x: 0,
-        y: 0,
-    },
-});
-
-const coordinatesObj = ref({
-    centerDelta: {
-        x: 0,
-        y: 0,
-    },
-})
-
-function createRandomRect() {
-    rectList.value.push({
-        x: Math.round(Math.random() * coordinatesObj.value.centerDelta.x * 2),
-        y: Math.round(Math.random() * coordinatesObj.value.centerDelta.y * 2),
-        width: Math.round(Math.random() * 50 + 50),
-        height: Math.round(Math.random() * 50 + 50),
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 2,
-    })
-}
-
-onMounted(() => {
-    configKonva.value = {
-        width: document.querySelector(".mainPage__mainBlock__canvas").clientWidth,
-        height: document.querySelector(".mainPage__mainBlock__canvas").clientHeight,
-    }
-
-    coordinatesObj.value.centerDelta.x = Math.round(configKonva.value.width / 2);
-    coordinatesObj.value.centerDelta.y = Math.round(configKonva.value.height / 2);
-
-    createRandomRect();
-
-    setTimeout(() => {
-        stage.value = stageRef.value.getStage();
-    }, 1000);
-})
-
-console.log("Created");
+import myCanvas from '@/components/myCanvas';
 </script>
 
 <style scoped lang="scss" src="./style.scss"></style>
