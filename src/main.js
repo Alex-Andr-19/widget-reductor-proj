d8769758fc1b9ed26c9a2236d84417a8bd78c05a
Alex-Andr-19 <andrey.aleksandrychev3@mail.ru>
2024-01-22 09:51:56 +0300

    Add sass + Remove useless module vue-konva

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import { createApp } from 'vue';
 import "./style.css";
 import App from './App.vue';
 import router from './router';
-import VueKonva from 'vue-konva';
 
 const Vue = createApp(App);
 
 const pinia = createPinia();
 Vue.use(pinia);
 Vue.use(router);
-Vue.use(VueKonva);
 
 Vue.mount('#app');
